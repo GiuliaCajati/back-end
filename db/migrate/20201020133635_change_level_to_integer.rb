dbadd283e78e732163c54e28c7bea8212a446a84
NavPoint1 <foleym156@gmail.com>
2020-10-26 09:19:27 -0700

    another postgres migration compatibility error

--- db/migrate/20201020133635_change_level_to_integer.rb
+++ db/migrate/20201020133635_change_level_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeLevelToInteger < ActiveRecord::Migration[6.0]
   def change 
-    change_column :relationships, :level, :integer
+    change_column :relationships, :level, 'integer USING CAST(column_name AS integer)'
   end
 end
